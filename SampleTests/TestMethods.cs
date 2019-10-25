5a58d5ccd3e0c00e157f41e27dc60b4d669d7052
Bruno Lins de Oliveira <brolivei@microsoft.com>
2019-10-25 14:00:41 -0700

    Adding test project

---
+++ SampleTests/TestMethods.cs
@@ -0,0 +1,26 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SampleTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.Fail();
+        }
+    }
+}
