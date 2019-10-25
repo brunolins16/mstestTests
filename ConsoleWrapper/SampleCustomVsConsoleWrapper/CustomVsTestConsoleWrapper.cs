12951f426de1b2015f0a096efb27bb0a846b90f7
Bruno Lins de Oliveira <brolivei@microsoft.com>
2019-10-25 15:22:01 -0700

    Adding custom wrapper

---
+++ ConsoleWrapper/SampleCustomVsConsoleWrapper/CustomVsTestConsoleWrapper.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.TestPlatform.VsTestConsole.TranslationLayer;
+
+namespace SampleCustomVsConsoleWrapper
+{
+    public class CustomVsTestConsoleWrapper : VsTestConsoleWrapper
+    {
+        public CustomVsTestConsoleWrapper(string vstestConsolePath) : base(vstestConsolePath)
+        {
+        }
+
+        public CustomVsTestConsoleWrapper(string vstestConsolePath, ConsoleParameters consoleParameters) : base(vstestConsolePath, consoleParameters)
+        {
+        }
+    }
+}
