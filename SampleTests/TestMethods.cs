10c4d17ae127f89a49e9e1244a0a1c747e19f247
Bruno Lins de Oliveira <brolivei@microsoft.com>
2019-10-28 15:55:10 -0700

    Adding priority

--- SampleTests/TestMethods.cs
+++ SampleTests/TestMethods.cs
@@ -6,24 +6,28 @@ namespace SampleTests
     public class UnitTest1
     {
         [TestMethod]
+        [Priority(0)]
         public void TestMethod1()
         {
             Assert.IsTrue(true);
         }
 
         [TestMethod]
+        [Priority(0)]
         public void TestMethod2()
         {
             Assert.Inconclusive();
         }
 
         [TestMethod]
+        [Priority(0)]
         public void TestMethod3()
         {
             Assert.Fail();
         }
 
         [TestMethod]
+        [Priority(0)]
         public void TestMethod4()
         {
             Assert.IsTrue(true);
