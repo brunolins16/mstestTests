d6dacb15847af64cd97ef974116766a2faff97c8
Bruno Lins de Oliveira <brolivei@microsoft.com>
2019-11-11 11:45:13 -0800

    Updating trait

--- XUnitSampleTests/TestMethods.cs
+++ XUnitSampleTests/TestMethods.cs
@@ -13,19 +13,19 @@ namespace XUnitSampleTests
         }
 
         [Fact(Skip = "Inconclusive")]
-        [Trait("Priority",0)]
+        [Trait("Priority","0")]
         public void TestMethod2()
         {}
 
         [Fact]
-        [Trait("Priority",0)]
+        [Trait("Priority","0")]
         public void TestMethod3()
         {
             Assert.True(false);
         }
 
         [Fact]
-        [Trait("Priority",0)]
+        [Trait("Priority","0")]
         public void TestMethod4()
         {
             Assert.True(true);
