86931b581531a3a981e2a23a800ffe03a1e70a26
Bruno Lins de Oliveira <brolivei@microsoft.com>
2019-11-11 11:35:26 -0800

    Adding priority

--- XUnitSampleTests/TestMethods.cs
+++ XUnitSampleTests/TestMethods.cs
@@ -6,22 +6,26 @@ namespace XUnitSampleTests
     public class TestMethods
     {
         [Fact]
+        [Trait("Priority",0)]
         public void TestMethod1()
         {
             Assert.True(true);
         }
 
         [Fact(Skip = "Inconclusive")]
+        [Trait("Priority",0)]
         public void TestMethod2()
         {}
 
         [Fact]
+        [Trait("Priority",0)]
         public void TestMethod3()
         {
             Assert.True(false);
         }
 
         [Fact]
+        [Trait("Priority",0)]
         public void TestMethod4()
         {
             Assert.True(true);
